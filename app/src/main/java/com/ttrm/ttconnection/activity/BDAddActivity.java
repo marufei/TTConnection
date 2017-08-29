0663c12de98545b9e0a0d299a736c315622a7fcb
marufei <867814102@qq.com>
2017-08-29 22:46:08 +0800

    首页

---
+++ app/src/main/java/com/ttrm/ttconnection/activity/BDAddActivity.java
@@ -0,0 +1,18 @@
+package com.ttrm.ttconnection.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ttrm.ttconnection.R;
+
+/**
+ * TODO 被动加粉
+ */
+public class BDAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bdadd);
+    }
+}
