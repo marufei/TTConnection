f0d8db62dabe5d048a9d37492ba93793f4a057ff
marufei <867814102@qq.com>
2017-09-04 17:43:48 +0800

    登录fragment

---
+++ app/src/main/java/com/ttrm/ttconnection/util/ActivityUtil.java
@@ -0,0 +1,45 @@
+package com.ttrm.ttconnection.util;
+
+import android.app.Activity;
+
+
+import java.util.ArrayList;
+
+public class ActivityUtil {
+	static String TAG ="TAG--ActivityUtil";
+
+	static ArrayList<Activity> mActivities = new ArrayList<Activity>();
+
+	public static void exitAll() {
+		for (Activity activity : mActivities) {
+			activity.finish();
+		}
+	}
+	public static void add(Activity activity) {
+		if (!mActivities.contains(activity)) {
+			mActivities.add(activity);
+		}
+//		MyUtils.Loge(TAG,"启动activity得个数="+mActivities.size()+"---activity顺序-0="+mActivities.get(0).toString()+"最后="+mActivities.get(mActivities.size()-1).toString());
+		getLastActivityName();
+	}
+	public static int select() {
+		return mActivities.size();
+	}
+	public static void delect(Activity activity){
+		if (mActivities.contains(activity)){
+			mActivities.remove(activity);
+		}
+//		MyUtils.Loge(TAG,"启动activity得个数="+mActivities.size());
+
+	}
+	public static String getLastActivityName(){
+//		MyUtils.Loge(TAG,"最后ActivityName="+mActivities.get(mActivities.size()-1).toString());
+		return mActivities.get(mActivities.size()-1).toString();
+
+	}
+/*	public static void toLogin(Activity activity){
+		Intent intent=new Intent(activity,LoginActivity.class);
+		intent.putExtra("logout",true);
+		activity.startActivity(intent);
+	}*/
+}
